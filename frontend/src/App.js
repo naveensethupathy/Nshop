195e2d53a9d76ac8995a4dfb9ed7897ed26e2d93
naveensethupathy <naveensethupathy1325@gmail.com>
2024-02-26 18:16:35 +0530

    Frontend on process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 const App = () => {
   return (
-    <h1>Nshop</h1>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  
   )
-}
+};
 
 export default App
